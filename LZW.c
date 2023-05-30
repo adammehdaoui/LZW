a54b1240c56db81cd54a7cefc06b57582e99130c
MehdaouiAdam <adam.mehdaouijorge@gmail.com>
2023-05-31 00:07:30 +0200

    format main args

--- LZW.c
+++ LZW.c
@@ -9,31 +9,26 @@
 #define NEXT_CODE 258
 #define MAX_CODE_SIZE 12
 
-#define NULL_CODE_9BITS 255
-#define NULL_CODE_10BITS 511
-#define NULL_CODE_11BITS 1023
-#define NULL_CODE_12BITS 2047
-
 #define BITS 12
 
 #define TAILLE_CHN_TEMP 24
 
 void compress(FILE *input_file,FILE *output_file) {
     BIT_FILE *bf = bit_begin(output_file);
-
     uint32_t next_code = NEXT_CODE;
     uint8_t input_symbol;
-    uint32_t chn[TAILLE_CHN_TEMP];
-    uint32_t last_valid[TAILLE_CHN_TEMP];
+    uint32_t * chn;
+    uint32_t * last_valid;
     int i;
     int j;
     int taille_last_valid=0;
     int taille_chn=0;
     uint32_t last_code = 0;
     int nombre_bits_courant = BITS;
-    int compteur_lignes = 0;
+    struct Trie* racine = CreationFeuille(-1);
 
-    struct Trie* head = CreationFeuille(-1);
+    chn = (uint32_t*)malloc(TAILLE_CHN_TEMP*sizeof(uint32_t));
+    last_valid = (uint32_t*)malloc(TAILLE_CHN_TEMP*sizeof(uint32_t));
 
     memset(chn,0,TAILLE_CHN_TEMP*sizeof(uint32_t));
     memset(last_valid,0,TAILLE_CHN_TEMP*sizeof(uint32_t));
@@ -43,21 +38,17 @@ void compress(FILE *input_file,FILE *output_file) {
         chn[0] = i;
         for (j=1;j<TAILLE_CHN_TEMP;j++)
             chn[j] = 0;
-        insert(head, chn ,i, 1);
+        insert(racine, chn ,i, 1);
     }  
 
     last_code = 0; 
     //fprintf(output_file,"%u ",256);
     bit_put(bf, 256, nombre_bits_courant);
-    
-    while ( ! feof(input_file)) { //while not end of file do
-        printf(".......................\n");
+     
+    while ( ! feof(input_file)) { //tant que la fin du fichier n'est pas atteinte
         input_symbol = getc(input_file);
-        compteur_lignes++;
-        //printf("input_symbol %d\n",input_symbol);
-        if (input_symbol == 255 && feof(input_file)) break;//indicateur de fin
-        //if (input_symbol == 0) input_symbol = NULL_CODE_9BITS;//indicateur de fin
-        printf("Lecture de %d ligne %d\n",input_symbol,compteur_lignes);
+        if (input_symbol == 255 && feof(input_file)) break;//indicateurs de fin
+        //printf("Lecture de %d\n",input_symbol);
         i=0;
 
         while(i<taille_last_valid)
@@ -67,34 +58,27 @@ void compress(FILE *input_file,FILE *output_file) {
             }
         chn[i] = input_symbol;
         i++;
-        printf("la\n");
-        while(i<TAILLE_CHN_TEMP)
-            {
-                chn[i] = 0 ;
-                i++;  
-            }
         taille_chn = taille_last_valid + 1;    
-        printf("Taille_chn %d Taille_last %d\n",taille_chn,taille_last_valid);
-        printf("Recherche de %u %u %u %u %u %u %u %u %u %u \n",chn[0],chn[1],chn[2],chn[3],chn[4],chn[5],chn[6],chn[7],chn[8],chn[9]);
-        printf("Recherche code Last code %d\n",Recherche_code_dans_l_arbre(head,chn,taille_chn));
+        //printf("Taille_chn %d Taille_last %d\n",taille_chn,taille_last_valid);
+        //printf("Recherche de %u %u %u %u %u %u %u %u %u %u \n",chn[0],chn[1],chn[2],chn[3],chn[4],chn[5],chn[6],chn[7],chn[8],chn[9]);
+        //printf("Recherche code Last code %d\n",Recherche_code_dans_l_arbre(racine,chn,taille_chn));
 
-        if (Recherche_code_dans_l_arbre(head,chn,taille_chn) != -1) 
+        if (Recherche_code_dans_l_arbre(racine,chn,taille_chn) != -1) 
         {
             // Le code existe dans le dictionnaire
             memcpy(last_valid,chn,TAILLE_CHN_TEMP*sizeof(uint32_t));
             taille_last_valid = taille_chn;
-            last_code = Recherche_code_dans_l_arbre(head, chn, taille_chn);
-            printf("%u %u %u %u %u %u %u %u %u %u Trouvé! Last_valid %u %u %u Last_code %d  \n",chn[0],chn[1],chn[2],chn[3],chn[4],chn[5],chn[6],chn[7],chn[8],chn[9],last_valid[0],last_valid[1],last_valid[2],last_code);
+            last_code = Recherche_code_dans_l_arbre(racine, chn, taille_chn);
+            //printf("%u %u %u %u %u %u %u %u %u %u Trouvé! Last_valid %u %u %u Last_code %d  \n",chn[0],chn[1],chn[2],chn[3],chn[4],chn[5],chn[6],chn[7],chn[8],chn[9],last_valid[0],last_valid[1],last_valid[2],last_code);
         } 
         else 
         {
-            // La sequence n existe pas: ajout au dictionnaire
-            insert(head, chn ,next_code, taille_chn);
-            printf("Ajouté %u %u %u %u %u %u %u %u %u %u en %d de taille %d\n",chn[0],chn[1],chn[2],chn[3],chn[4],chn[5],chn[6],chn[7],chn[8],chn[9],next_code,taille_chn);
+            // Le code n existe pas: ajout au dictionnaire
+            insert(racine, chn ,next_code, taille_chn);
+            //printf("Ajouté %u %u %u %u %u %u %u %u %u %u en %d de taille %d\n",chn[0],chn[1],chn[2],chn[3],chn[4],chn[5],chn[6],chn[7],chn[8],chn[9],next_code,taille_chn);
             next_code++;
-            printf("next_code %d\n",next_code);
 
-            printf("ECRITURE %u\n",last_code);
+            //printf("ECRITURE %u\n",last_code);
             bit_put(bf, last_code, nombre_bits_courant); 
 
             last_valid[0] = input_symbol;
@@ -102,14 +86,14 @@ void compress(FILE *input_file,FILE *output_file) {
             taille_last_valid = 1;
             taille_chn = 1;
 
-            for (i=1;i<TAILLE_CHN_TEMP;i++){
+            /*for (i=1;i<TAILLE_CHN_TEMP;i++){
                 chn[i] = 0;
                 last_valid[i] = 0;
-            }
+            }*/
             last_code = input_symbol;
-            printf("%u %u %u %u %u %u %u %u %u %u Pas trouvé: Last valid %u %u %u Last code %d  Next code %d\n",chn[0],chn[1],chn[2],chn[3],chn[4],chn[5],chn[6],chn[7],chn[8],chn[9],last_valid[0],last_valid[1],last_valid[2],last_code,next_code);
+            //printf("%u %u %u %u %u %u %u %u %u %u Pas trouvé: Last valid %u %u %u Last code %d  Next code %d\n",chn[0],chn[1],chn[2],chn[3],chn[4],chn[5],chn[6],chn[7],chn[8],chn[9],last_valid[0],last_valid[1],last_valid[2],last_code,next_code);
         }
-        printf("Taille_chn %d Taille_last %d\n",taille_chn,taille_last_valid);
+        
         
     }
     //fprintf(output_file,"%u ",last_code);
@@ -120,7 +104,9 @@ void compress(FILE *input_file,FILE *output_file) {
     bit_flush(bf);
     bit_end(bf);
 
-    Liberation(head);
+    Liberation(racine);
+    free(chn);
+    free(last_valid);
 }
 
 void decompress(FILE *input_file,FILE *output_file) {
@@ -128,20 +114,22 @@ void decompress(FILE *input_file,FILE *output_file) {
     uint32_t next_code = NEXT_CODE;
     int i, j;
     int currentNumber;
-    uint32_t last_valid[TAILLE_CHN_TEMP];
-    uint32_t seq[TAILLE_CHN_TEMP];
-    uint32_t chn[TAILLE_CHN_TEMP];
+    uint32_t * last_valid;
+    uint32_t * seq;
+    uint32_t * chn;
     int taille_last_valid=0;
     int taille_seq=0;
     int taille_chn=1;
     int Trouve=0;
     uint32_t code;
     BIT_FILE *bf = bit_begin(input_file);
-    struct Trie* head = CreationFeuille(-1);
+    struct Trie* racine = CreationFeuille(-1);
     int nombre_bits_courant = BITS;
-    int CompteurLignes = 0;
     int PremierCaractere = 1;
 
+    chn = (uint32_t*)malloc(TAILLE_CHN_TEMP*sizeof(uint32_t));
+    last_valid = (uint32_t*)malloc(TAILLE_CHN_TEMP*sizeof(uint32_t));
+    seq = (uint32_t*)malloc(TAILLE_CHN_TEMP*sizeof(uint32_t));
     memset(chn,0,TAILLE_CHN_TEMP*sizeof(uint32_t));
     memset(last_valid,0,TAILLE_CHN_TEMP*sizeof(uint32_t));
     memset(seq,0,TAILLE_CHN_TEMP*sizeof(uint32_t));
@@ -151,22 +139,20 @@ void decompress(FILE *input_file,FILE *output_file) {
         chn[0] = i;
         for (j=1;j<TAILLE_CHN_TEMP;j++)
             chn[j] = 0;
-        insert(head, chn ,i, 1);
+        insert(racine, chn ,i, 1);
     }  
     
    //une boucle qui prend chaque ligne les unes apres les autres jusqu'à la fin du fichier
     while (bit_get(bf, &code, nombre_bits_courant) != EOF) {
-            printf(".......................\n");
             currentNumber = code;
-            CompteurLignes++;
 
             if (currentNumber != 257 && currentNumber != 256)
             {
-                printf("Analyse du %d avec %d bits en ligne %d\n",currentNumber,nombre_bits_courant,CompteurLignes);
+                //printf("Analyse du %d avec %d bits\n",currentNumber,nombre_bits_courant);
                 memset(seq,0,TAILLE_CHN_TEMP*sizeof(uint32_t));
-                Trouve = Recherche_un_noeud(head,currentNumber,*(&seq));
-                printf("retour de recherche seq %u %u Trouve %d\n",seq[0],seq[1],Trouve);
-                taille_seq = CompteurNiveaux(head, Recherche_dans_l_arbre(head,currentNumber))-1;
+                Trouve = Recherche_un_noeud(racine,currentNumber,*(&seq));
+                //printf("retour de recherche seq %u %u Trouve %d\n",seq[0],seq[1],Trouve);
+                taille_seq = CompteurNiveaux(racine, Recherche_dans_l_arbre(racine,currentNumber))-1;
 
                 if (Trouve != 0){ //Trouvé
 
@@ -174,15 +160,15 @@ void decompress(FILE *input_file,FILE *output_file) {
                     memcpy(chn,last_valid,taille_last_valid*sizeof(uint32_t));
                     chn[taille_last_valid] = seq[0];
                     taille_chn = taille_last_valid + 1;
-                    printf("Taille chn %d\n",taille_chn);
+                    //printf("Taille chn %d\n",taille_chn);
                     if (PremierCaractere == 1) 
                     { 
                         PremierCaractere = 0; 
                     }
                     else
                     {
-                        insert(head,chn,next_code,taille_chn);
-                        printf("Ajout dico en:%d chn:%u %u %u %u taille %d\n",next_code,chn[0],chn[1],chn[2],chn[3],taille_chn);
+                        insert(racine,chn,next_code,taille_chn);
+                        //printf("Ajout dico en:%d chn:%u %u %u %u taille %d\n",next_code,chn[0],chn[1],chn[2],chn[3],taille_chn);
                         next_code++;
                     }
                     
@@ -192,10 +178,10 @@ void decompress(FILE *input_file,FILE *output_file) {
 
                     for (i=0;i<taille_seq;i++)
                         {
-                            printf("ECRITURE %u\n", seq[i]);
+                            //printf("ECRITURE %u\n", seq[i]);
                             fprintf(output_file,"%c", seq[i]); 
                         }  
-                    printf("%d Trouvé! last:%u %u %u seq:%u %u %u %u next:%d\n",currentNumber,last_valid[0],last_valid[1],last_valid[2],seq[0],seq[1],seq[2],seq[3],next_code);
+                    //printf("%d Trouvé! last:%u %u %u seq:%u %u %u %u next:%d\n",currentNumber,last_valid[0],last_valid[1],last_valid[2],seq[0],seq[1],seq[2],seq[3],next_code);
                 } 
                 else //Pas trouvé
                 {
@@ -206,9 +192,9 @@ void decompress(FILE *input_file,FILE *output_file) {
                     memcpy(chn,last_valid,taille_last_valid*sizeof(uint32_t));   
                     chn[taille_last_valid] = seq[0];
                     taille_chn = taille_last_valid + 1;
-                    insert(head,chn,next_code, taille_chn);
+                    insert(racine,chn,next_code, taille_chn);
                 
-                    printf("Ajout dico next:%d chn:%u %u %u taille %d\n",next_code,chn[0],chn[1],chn[2],taille_chn);
+                    //printf("Ajout dico next:%d chn:%u %u %u taille %d\n",next_code,chn[0],chn[1],chn[2],taille_chn);
                     next_code++;
                     /*if (next_code == 512) nombre_bits_courant = 10;
                     if (next_code == 1024) nombre_bits_courant = 11;
@@ -219,10 +205,10 @@ void decompress(FILE *input_file,FILE *output_file) {
                     taille_last_valid = taille_seq;  
                     for (i=0;i<taille_seq;i++)
                     {
-                        printf("%u\n", seq[i]);
+                        //printf("%u\n", seq[i]);
                         fprintf(output_file,"%c", seq[i]); 
                     }     
-                    printf("%d Pas Trouvé! last:%u %u %u seq:%u %u %u next:%d\n",currentNumber,last_valid[0],last_valid[1],last_valid[2],seq[0],seq[1],seq[2],next_code);
+                    //printf("%d Pas Trouvé! last:%u %u %u seq:%u %u %u next:%d\n",currentNumber,last_valid[0],last_valid[1],last_valid[2],seq[0],seq[1],seq[2],next_code);
                 }
             }       
     }
@@ -230,39 +216,70 @@ void decompress(FILE *input_file,FILE *output_file) {
     bit_flush(bf);
     bit_end(bf);
 
-    printf("Nombre de noeuds : %d\n", CompteurNoeuds(head));
-    Liberation(head);
+    //printf("Nombre de noeuds : %d\n", CompteurNoeuds(racine));
+    Liberation(racine);
+    free(chn);
+    free(last_valid);
+    free(seq);
 }
 
-int main() {
-    FILE *input_file = fopen("comp_input.txt", "rb");
-    if (input_file == NULL) {
-        fprintf(stderr, "Error opening file\n");
+int main(int argc, char* argv[]) {
+    FILE *input_file;
+    FILE *output_file;
+    char * output_file_name;
+
+    if (argc > 2) {
+    } else {
+        printf("Format: ./LZW (encode or decode) NomFichier\n");
         exit(1);
     }
-    FILE *output_file = fopen("comp_output.txt", "w");
-    if (output_file == NULL) {
-        fprintf(stderr, "Error opening file\n");
-        exit(1);
-    } 
-    compress(input_file, output_file);
 
-    fclose(input_file);
-    fclose(output_file);
-    printf("--------------------\n");
-    input_file = fopen("comp_output.txt", "rb");
-    if (input_file == NULL) {
-        fprintf(stderr, "Error opening file\n");
+    if (strcmp(argv[1] , "encode") == 0 || strcmp(argv[1] , "decode") == 0) {
+    }
+    else {
+        printf("Format: ./LZW (encode or decode) NomFichier\n");
         exit(1);
     }
-    output_file = fopen("decomp_output.txt", "w");
-    if (output_file == NULL) {
-        fprintf(stderr, "Error opening file\n");
-        exit(1);
-    } 
 
-    decompress(input_file,output_file);
+    output_file_name = (char*)malloc(strlen(argv[2]) + 10);
+    if (strcmp(argv[1] , "encode") == 0) strcpy(output_file_name, "encode_");
+    else strcpy(output_file_name, "decode_");
+    strcat(output_file_name, argv[2]);
 
-    fclose(input_file);
-    fclose(output_file);
+    if (strcmp(argv[1] , "encode") == 0) {
+        input_file = fopen(argv[2], "rb");
+        if (input_file == NULL) {
+            fprintf(stderr, "Erreur d ouverture de fichier\n");
+            exit(1);
+        }
+        output_file = fopen(output_file_name, "w");
+        if (output_file == NULL) {
+            fprintf(stderr, "Erreur d ouverture de fichier\n");
+            exit(1);
+        } 
+        compress(input_file, output_file);
+
+        fclose(input_file);
+        fclose(output_file);
+    }
+
+    if (strcmp(argv[1] , "decode") == 0) {
+        input_file = fopen(argv[2], "rb");
+        if (input_file == NULL) {
+            fprintf(stderr, "Erreur d ouverture de fichier\n");
+            exit(1);
+        }
+        output_file = fopen(output_file_name, "w");
+        if (output_file == NULL) {
+            fprintf(stderr, "Erreur d ouverture de fichier\n");
+            exit(1);
+        } 
+
+        decompress(input_file,output_file);
+
+        fclose(input_file);
+        fclose(output_file);
+    }
+
+    free(output_file_name);
 }
--- arbre_dico.c
+++ arbre_dico.c
@@ -17,14 +17,12 @@ struct Trie* CreationFeuille(int code)
 }
  
 // Fonction itérative pour insérer une string dans un Trie
-void insert(struct Trie *head, uint32_t * str, int code, int taille)
+void insert(struct Trie *racine, uint32_t * str, int code, int taille)
 {
     // partir du nœud racine
-    struct Trie* curr = head;
+    struct Trie* curr = racine;
     int i=0;
     int compteur = taille;
-
-    printf("creation noeud %u %u %u %u %u %u %u %u %u %u\n",str[0],str[1],str[2],str[3],str[4],str[5],str[6],str[7],str[8],str[9]);
 
     while (compteur  > 0)
     {
@@ -45,16 +43,16 @@ void insert(struct Trie *head, uint32_t * str, int code, int taille)
  
 // Fonction itérative pour rechercher une string dans un Trie. Il renvoie 1
 // si la string est trouvée dans le Trie ; sinon, il renvoie 0.
-int Recherche(struct Trie* head, uint32_t * str)
+int Recherche(struct Trie* racine, uint32_t * str)
 {
     int i=0;
     
     // retourne 0 si Trie est vide
-    if (head == NULL) {
+    if (racine == NULL) {
         return 0;
     }
  
-    struct Trie* curr = head;
+    struct Trie* curr = racine;
     while (str[i])
     {
         curr = curr->character[str[i]];
@@ -68,27 +66,27 @@ int Recherche(struct Trie* head, uint32_t * str)
 }
 
 
- int Recherche_code_dans_l_arbre(struct Trie* head, uint32_t* str, int taille)
+// Fonction itérative pour rechercher un code dans un Trie. Il renvoie le code
+// si la string est trouvée dans le Trie ; sinon, il renvoie -1.
+ int Recherche_code_dans_l_arbre(struct Trie* racine, uint32_t* str, int taille)
 {
     int i=0;
     int compteur = taille;
     
     // retourne 0 si Trie est vide
-    if (head == NULL) {
-        printf("head null\n");
+    if (racine == NULL) {
         return -1;
     }
  
-    struct Trie* curr = head;
+    struct Trie* curr = racine;
     while (compteur>0)
     {
-        printf("while %d code %d compt %d\n",i, curr->code_ascii,compteur);
+        //printf("while %d code %d compt %d\n",i, curr->code_ascii,compteur);
         // passe au nœud suivant
         curr = curr->character[str[i]];
     
         // si la string est invalide (a atteint la fin d'un chemin dans le Trie)
         if (curr == NULL) {
-            printf("on sort ici\n");
             return -1;
         }
  
@@ -99,7 +97,6 @@ int Recherche(struct Trie* head, uint32_t * str)
  
     // renvoie 1 si le noeud courant est une feuille et que le
     // la fin de la string est atteinte
-    printf("on arrive ici\n");
     return curr->code_ascii;
 }
  
@@ -116,11 +113,10 @@ int A_un_enfant(struct Trie* curr)
     return 0;
 }
 
-// Fonction récursive pour rechercher un code ASCII dans un Trie et renvoyer la chaîne correspondante
+// Fonction récursive pour rechercher un code dans un Trie et renvoyer la chaîne correspondante
 struct Trie* Recherche_dans_l_arbre(struct Trie* node, int code)
 {
     if (node == NULL) {
-        printf("node null\n");
         return NULL;
     }
 
@@ -129,7 +125,6 @@ struct Trie* Recherche_dans_l_arbre(struct Trie* node, int code)
     // Si le nœud courant est une feuille et son code correspond au code recherché
     if (node->code_ascii == code)
     {
-        printf("sortie trouvé\n");
         return node;
     }
 
@@ -151,7 +146,7 @@ struct Trie* Recherche_dans_l_arbre(struct Trie* node, int code)
 }
 
 // Fonction pour rechercher un code ASCII dans un Trie et renvoyer la chaîne correspondante
-int Recherche_un_noeud(struct Trie* head, int code, uint32_t* str)
+int Recherche_un_noeud(struct Trie* racine, int code, uint32_t* str)
 {
     struct Trie* node;
     struct Trie* node_enfant;
@@ -161,75 +156,75 @@ int Recherche_un_noeud(struct Trie* head, int code, uint32_t* str)
     int i = 0;
     uint32_t chn[24];
     
-    if (head == NULL ) {
-        printf("head null\n");
+    if (racine == NULL ) {
         return 0;
     }    
     
     memset(chn,0,24*sizeof(uint32_t));
-    node = Recherche_dans_l_arbre(head, code);
-    printf("node %p \n",node);
+    node = Recherche_dans_l_arbre(racine, code);
     
-    i = CompteurNiveaux(head,node)-2;
+    i = CompteurNiveaux(racine,node)-2;
     node_enfant = node;
     while(node_enfant != NULL){
-        node_parent = Trouver_Noeud_Pere(head, node_enfant);
+        node_parent = Trouver_Noeud_Pere(racine, node_enfant);
         enieme = Lien_character_pere_fils(node_parent, node_enfant);
         chn[i] = enieme;
         node_enfant = node_parent;
         i--;
     }
-    printf("chn %u %u\n",chn[0],chn[1]);
+    
     if (node != NULL) {
-        printf("on passe dans le if\n");
-        memcpy(str,chn, (CompteurNiveaux(head,node)-1) * sizeof(uint32_t));
+        memcpy(str,chn, (CompteurNiveaux(racine,node)-1) * sizeof(uint32_t));
         return 1;
     }
     else return 0;
 
 }
 
-int CompteurNoeuds(struct Trie* head)
+// Fonction récursive pour compter le nombre de nœuds dans un Trie
+int CompteurNoeuds(struct Trie* racine)
 {
-    if (head == NULL) {
+    if (racine == NULL) {
         return 0;
     }
 
     int count = 1; // Compte le nœud courant
 
     for (int i = 0; i < CHAR_SIZE; i++) {
-        if (head->character[i] != NULL)
-            count += CompteurNoeuds(head->character[i]); // Compte les nœuds des enfants récursivement
+        if (racine->character[i] != NULL)
+            count += CompteurNoeuds(racine->character[i]); // Compte les nœuds des enfants récursivement
     }
 
     return count;
 }
 
-void Liberation(struct Trie* head)
+// Fonction récursive pour libérer la mémoire allouée à un Trie
+void Liberation(struct Trie* racine)
 {
     for (int i = 0; i < CHAR_SIZE; i++) {
-        if (head->character[i] != NULL)
-            Liberation(head->character[i]);
+        if (racine->character[i] != NULL)
+            Liberation(racine->character[i]);
     }
 
-    if (head != NULL)
+    if (racine != NULL)
     {
-        free(head);
+        free(racine);
     }
   
 }
 
-struct Trie* Trouver_Noeud_Pere(struct Trie* head, struct Trie* child)
+// Fonction récursive pour trouver le nœud père d'un nœud donné
+struct Trie* Trouver_Noeud_Pere(struct Trie* racine, struct Trie* fils)
 {
-    if (head == NULL || child == NULL)
+    if (racine == NULL || fils == NULL)
         return NULL;
     
     for (int i = 0; i < CHAR_SIZE; i++) {
-        if (head->character[i] == child)
-            return head;
+        if (racine->character[i] == fils)
+            return racine;
         
-        if (head->character[i] != NULL) {
-            struct Trie* node = Trouver_Noeud_Pere(head->character[i], child);
+        if (racine->character[i] != NULL) {
+            struct Trie* node = Trouver_Noeud_Pere(racine->character[i], fils);
             if (node != NULL)
                 return node;
         }
@@ -238,10 +233,10 @@ struct Trie* Trouver_Noeud_Pere(struct Trie* head, struct Trie* child)
     return NULL;
 }
 
+// Fonction récursive pour trouver le lien Character entre un nœud père et un nœud fils
 int Lien_character_pere_fils(struct Trie* pere, struct Trie* fils)
 {
     if (fils == NULL || pere == NULL) {
-        //printf("on passe dans le if\n");
         return 0;
     }   
 
@@ -255,18 +250,19 @@ int Lien_character_pere_fils(struct Trie* pere, struct Trie* fils)
     return 0;
 }
 
-int CompteurNiveaux(struct Trie * head,struct Trie * node){
+// Fonction récursive pour compter le nombre de niveaux entre un nœud et la racine
+int CompteurNiveaux(struct Trie * racine,struct Trie * node){
     int i=0;
     struct Trie * node_enfant;
     struct Trie * node_parent;
     
-    if (head == NULL || node == NULL) {
+    if (racine == NULL || node == NULL) {
         return 0;
     }    
     
     node_enfant = node;
     while(node_enfant != NULL){
-        node_parent = Trouver_Noeud_Pere(head, node_enfant);
+        node_parent = Trouver_Noeud_Pere(racine, node_enfant);
         node_enfant = node_parent;
         i++;
     }
